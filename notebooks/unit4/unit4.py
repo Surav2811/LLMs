36d8d1bb25e36a1b0d94f3e9af8c30d9c220cf9a
Saurav Mishra <69195216+Surav2811@users.noreply.github.com>
2025-02-06 16:13:36 +0530

    LangChain and LangGraph for RAG from webpage

---
+++ notebooks/unit4/unit4.py
@@ -0,0 +1,126 @@
+import getpass
+import os
+from langchain_ollama import OllamaEmbeddings
+from langchain_groq import ChatGroq
+from langchain_core.vectorstores import InMemoryVectorStore
+import bs4
+from langchain import hub
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from typing_extensions import List, TypedDict
+from langgraph.graph import MessagesState, StateGraph
+from langchain_core.tools import tool
+from langchain_core.messages import SystemMessage
+from langgraph.prebuilt import ToolNode
+from langgraph.graph import END
+from langgraph.prebuilt import ToolNode, tools_condition
+from IPython.display import Image, display
+
+
+
+if not os.environ.get("GROQ_API_KEY"):
+  os.environ["GROQ_API_KEY"] = getpass.getpass("Enter API key for Groq: ")
+
+embeddings = OllamaEmbeddings(model="llama3")
+model = ChatGroq(model="llama3-8b-8192")
+vector_store = InMemoryVectorStore(embeddings)
+
+# Load and chunk contents of the blog
+loader = WebBaseLoader(
+    web_paths=("https://huggingface.co/learn/deep-rl-course/unit4/what-are-policy-based-methods",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    ),
+)
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+#print(text_splitter.split_documents(docs))
+all_splits = text_splitter.split_documents(docs)
+# Index chunks
+_ = vector_store.add_documents(documents=all_splits)
+graph_builder = StateGraph(MessagesState)
+@tool(response_format="content_and_artifact")
+def retrieve(query: str):
+    """Retrieve information related to a query."""
+    retrieved_docs = vector_store.similarity_search(query, k=2)
+    serialized = "\n\n".join(
+        (f"Source: {doc.metadata}\n" f"Content: {doc.page_content}")
+        for doc in retrieved_docs
+    )
+    return serialized, retrieved_docs
+
+# Step 1: Generate an AIMessage that may include a tool-call to be sent.
+def query_or_respond(state: MessagesState):
+    """Generate tool call for retrieval or respond."""
+    llm_with_tools = model.bind_tools([retrieve])
+    response = llm_with_tools.invoke(state["messages"])
+    # MessagesState appends messages to state instead of overwriting
+    return {"messages": [response]}
+
+
+# Step 2: Execute the retrieval.
+tools = ToolNode([retrieve])
+
+
+# Step 3: Generate a response using the retrieved content.
+def generate(state: MessagesState):
+    """Generate answer."""
+    # Get generated ToolMessages
+    recent_tool_messages = []
+    for message in reversed(state["messages"]):
+        if message.type == "tool":
+            recent_tool_messages.append(message)
+        else:
+            break
+    tool_messages = recent_tool_messages[::-1]
+
+    # Format into prompt
+    docs_content = "\n\n".join(doc.content for doc in tool_messages)
+    system_message_content = (
+        "You are an assistant for question-answering tasks. "
+        "Use the following pieces of retrieved context to answer "
+        "the question. If you don't know the answer, say that you "
+        "don't know. Use three sentences maximum and keep the "
+        "answer concise."
+        "\n\n"
+        f"{docs_content}"
+    )
+    conversation_messages = [
+        message
+        for message in state["messages"]
+        if message.type in ("human", "system")
+        or (message.type == "ai" and not message.tool_calls)
+    ]
+    prompt = [SystemMessage(system_message_content)] + conversation_messages
+
+    # Run
+    response = model.invoke(prompt)
+    return {"messages": [response]}
+
+graph_builder.add_node(query_or_respond)
+graph_builder.add_node(tools)
+graph_builder.add_node(generate)
+
+graph_builder.set_entry_point("query_or_respond")
+graph_builder.add_conditional_edges(
+    "query_or_respond",
+    tools_condition,
+    {END: END, "tools": "tools"},
+)
+graph_builder.add_edge("tools", "generate")
+graph_builder.add_edge("generate", END)
+
+graph = graph_builder.compile()
+display(Image(graph.get_graph().draw_mermaid_png()))
+
+input_message = "Hello"
+
+for step in graph.stream(
+    {"messages": [{"role": "user", "content": input_message}]},
+    stream_mode="values",
+):
+    step["messages"][-1].pretty_print()
